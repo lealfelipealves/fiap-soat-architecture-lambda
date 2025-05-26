ea1706c26bdf5bbafd1003f3f1efd828d8f09ccc
Felipe Leal <lealfelipealves@gmail.com>
2025-05-26 08:30:07 -0300

    fix: remove profile

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,10 +2,6 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "aws_profile" {
-  default = "fiap-sso"
-}
-
 variable "prefix" {
   description = "Prefixo usado para nomear recursos"
   type        = string
